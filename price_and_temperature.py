552b90769ecc326b5ac9977be4c443a77a1b8c95
Nathan-023 <nathanli2023@lee-sfaa.com>
2022-01-28 20:58:48 +0800

    test_integers_and_floats.py

--- price_and_temperature.py
+++ price_and_temperature.py
@@ -6,12 +6,13 @@ def unit_price():
     # the user will input a price (¥), and a weight (kg). This function should return the price per kilogram
     price = input("What is the price? (¥) ")
     weight = input("What is the product weight? (kg) ")
-    return f"This product is ¥{1/1}/kg"
+    unit_price = float(price) / float (weight)
+    return f"This product is ¥{unit_price}/kg"
 
 
 def celsius_to_fahrenheit():
     # function requirements:
     # the user will input a temperature in celsius. This function should return the temperature in Fahrenheit
     celsius = input("What's the temperature in °C? ")
-    farenheit = (9 / 5) * celsius + 32
-    return f"°C is °F"
+    fahrenheit = (9 / 5) * float(celsius) + 32
+    return f"{celsius}°C is {fahrenheit}°F"
--- test_price_and_temperature.py
+++ test_price_and_temperature.py
@@ -4,7 +4,7 @@ from price_and_temperature import unit_price, celsius_to_fahrenheit
 
 class Test(TestCase):
     def test_unit_price(self):
-        self.assertEqual(unit_price(), f"This product is ¥ /kg")
+        self.assertEqual(unit_price(), f"This product is ¥{60/30}/kg")
 
     def test_celsius_to_fahrenheit(self):
-        self.assertEqual(celsius_to_fahrenheit(), f" °C is  °F")
+        self.assertEqual(celsius_to_fahrenheit(), f"{50}°C is {122.0}°F")
